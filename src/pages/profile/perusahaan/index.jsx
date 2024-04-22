2a8e0aa70a5b314ed00896b5252009daf5919315
hasbiriza <hasbiriza@gmail.com>
2024-04-22 12:17:03 +0700

    cek123

--- src/pages/profile/perusahaan/index.jsx
+++ src/pages/profile/perusahaan/index.jsx
@@ -6,12 +6,16 @@ import Navbar1 from "@/components/Navbar/Navbar";
 import Image from "next/image"; // Import Image dari next/image
 import miniprofile from "@/assets/img/miniprofile.png";
 import pp from "@/assets/img/Harry.png";
-import logophone from "@/assets/img/logophone.png";
-import logomail from "@/assets/img/logomail.png";
-import logolinkedin from "@/assets/img/logolinkedin.png";
-import logoinstagram from "@/assets/img/logoinstagram.png";
 import Footer from "@/components/Footer/Footer";
 import { Button } from "react-bootstrap";
+
+import {
+  FaMapMarkerAlt,
+  FaInstagram,
+  FaLinkedin,
+  FaRegEnvelope,
+  FaPhoneAlt,
+} from "react-icons/fa";
 
 function ProfilePerekrut() {
   const fallbackImage = "/Make Your Day.jpg";
@@ -32,39 +36,65 @@ function ProfilePerekrut() {
 
   return (
     <>
-      <Navbar1/>
+      <Navbar1 />
       <div className="container border border-danger mt-4 d-flex flex-column align-items-center ">
-      <div
-        className="rounded"
-        style={{
-          backgroundColor: "#5E50A1",
-          width: "100%",
-          height: "30vh",
-          zIndex: "0",
-          position: "relative",
-        }}
-      />
-      <Image src={pp} alt="pp" className="my-3 img-fluid border border-danger position-relative  " 
-      style={{
-      top:"-80px"
-      }}/>
+        <div
+          className="rounded"
+          style={{
+            backgroundColor: "#5E50A1",
+            width: "100%",
+            height: "30vh",
+            zIndex: "0",
+            position: "relative",
+          }}
+        />
+        <Image
+          src={pp}
+          alt="pp"
+          className="my-3 img-fluid border border-danger position-relative  "
+          style={{
+            top: "-80px",
+          }}
+        />
 
-      <div className=" w-75 border border-danger d-flex flex-column  align-items-center ">
-        <h3 className=" d-block ">PT.MARTABAT JAYA ABADI</h3>
-        <p className=" d-block">Financial</p>
-        <p className=" d-block">Alamat</p>
-        <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam modi molestias sed in voluptatum voluptate repellat magnam minima pariatur beatae!</p>
-        <div className="border border-danger"> <Image src={logomail} alt="logomail"/>Mantap 
-           <p className="ms-5">mantap pool</p>
-            </div>
+        <div className=" w-75 border border-danger d-flex flex-column  align-items-center ">
+          <h3 className=" d-block ">PT.MARTABAT JAYA ABADI</h3>
+          
+          <div>
+            <small>
+              <FaMapMarkerAlt /> Alamat
+            </small>
+          </div>
 
-        <Button className="w-25 BgSecondaryColor">Edit Profile</Button>
+          <div className="my-2">
+            <small>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero
+              consectetur natus quo nihil illum ea!
+            </small>
+          </div>
+
+          <Button className="w-25 BgSecondaryColor mt-2">Edit Profile</Button>
+
+          {/* Bagian2 Logo */}
+          <div className="text-secondary mt-4 ">
+            <FaRegEnvelope /> <small className="ms-1">Cek123</small>
+          </div>
+
+          <div className="text-secondary mt-1">
+            <FaInstagram /> <small className="ms-1">asdasd</small>
+          </div>
+
+          <div className="text-secondary mt-1">
+            <FaPhoneAlt /> <small className="ms-1">Github</small>
+          </div>
+
+          <div className="text-secondary mt-1">
+            <FaLinkedin /> <small className="ms-1">LINKEDIN</small>
+          </div>
+        </div>
+
+        <Footer />
       </div>
-      
-      <Footer/>
-      </div>
-      
-      
     </>
   );
 }
